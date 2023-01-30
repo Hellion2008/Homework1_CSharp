2c9a4138c6959a3b3e2ae69286f12390dfc629c8
Sergey <semakin.s.v@gmail.com>
2023-01-30 15:25:42 +0300

    fixed task36

--- task36/Program.cs
+++ task36/Program.cs
@@ -12,12 +12,9 @@ void ShowSum(int[] arr)
 int CalculateSum(int[] arr)
 {
   int sum = 0;
-  for (int i = 0; i < arr.Length; i++)
+  for (int i = 1; i < arr.Length; i += 2)
   {
-    if (i % 2 != 0)
-    {
       sum += arr[i];
-    } 
   }
   return sum;
 }
