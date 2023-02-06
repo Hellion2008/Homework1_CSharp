efe5ab6496436725798deb3c27f462e3ac93af2b
Sergey <semakin.s.v@gmail.com>
2023-02-06 13:30:13 +0300

    fixed task50

--- task50/Program.cs
+++ task50/Program.cs
@@ -5,26 +5,25 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-void ControlNumber(int[,] array, int pos)
+void ControlNumber(int[,] array, int posRow, int posColumn)
 {
   int rows = array.GetLength(0);
   int cols = array.GetLength(1);
-  if (pos < rows * cols)
+  if (posRow < rows && posColumn < cols)
   {
-    Console.WriteLine($"i = {pos / cols} , j = {pos % cols}");
-    Console.WriteLine(array[pos / cols, pos % cols]);
+    Console.WriteLine($"Number {array[posRow, posColumn]} stand on position [{posRow}, {posColumn}]");
   }
-  else Console.WriteLine($"Matrix hasn't got position {pos}"); 
+  else Console.WriteLine($"Matrix hasn't got position [{posRow}, {posColumn}]");
 }
 
 void ShowMatrix(int[,] matrix)
 {
-  
+
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-      Console.Write(matrix[i,j] + "\t");
+      Console.Write(matrix[i, j] + "\t");
     }
     Console.WriteLine();
   }
@@ -42,12 +41,12 @@ int[,] GetMatrix()
   Console.WriteLine("Enter matrix MxN:");
   int m = GetNumber("M = ");
   int n = GetNumber("N = ");
-  int[,] arr = new int[m,n];
+  int[,] arr = new int[m, n];
   for (int i = 0; i < m; i++)
   {
     for (int j = 0; j < n; j++)
     {
-      arr[i,j] = new Random().Next(1,100);
+      arr[i, j] = new Random().Next(1, 100);
     }
   }
   return arr;
@@ -55,5 +54,6 @@ int[,] GetMatrix()
 
 int[,] matrix = GetMatrix();
 ShowMatrix(matrix);
-int controlPos = GetNumber("Enter position in matrix for control: ");
-ControlNumber(matrix, controlPos);
+int posRow = GetNumber("Enter position in matrix's row for control: ");
+int posCol = GetNumber("Enter position in matrix's column  for control: ");
+ControlNumber(matrix, posRow, posCol);
